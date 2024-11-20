2a7747c59847238d9436ea4378a5c19928949f9d
Jeroen de Vries <jeroen@maildevries.eu>
2024-11-20 12:23:45 +0100

    reformat with commit format off

--- macros/staging.sql
+++ macros/staging.sql
@@ -3,15 +3,16 @@
 -- Extract only on latest ID if there is a full copy. 
 -- Combination of ID where timestamp is greater
 {% macro id_on_latest(id, extraction, table) %}
-select {{ id }}, max({{ extraction }}) as latest_extraction_at
-from {{ table }}
-group by {{ id }}
-    {% endmacro %}
+    select {{ id }}, max({{ extraction }}) as latest_extraction_at
+    from {{ table }}
+    group by {{ id }}
+{% endmacro %}
 
-    {% macro filter_id_latest(id, extraction, table) %} inner join
+{% macro filter_id_latest(id, extraction, table) %}
+    inner join
         ({{ id_on_latest(id, extraction, table) }}) as latest_extraction
-on latest_extraction.latest_extraction_at = {{ table }}.extracted_at
+        on latest_extraction.latest_extraction_at = {{ table }}.extracted_at
 
-order by {{ table }}.{{ id }} desc
+    order by {{ table }}.{{ id }} desc
 
 {% endmacro %}
